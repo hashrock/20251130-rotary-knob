addce2fcc3a3b690822139aee362867313d45eef
hashrock <hashrock@users.noreply.github.com>
2025-12-13 15:25:26 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/20251130-rotary-knob/',
+  base: '/20251130-guiland-rotary-knob/',
 })
